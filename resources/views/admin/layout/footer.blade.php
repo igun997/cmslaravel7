66f1abdefc9012b29679febe3cfcd0b1a99e6c83
Indra Gunanda <indra.gunanda@gmail.com>
2021-04-25 00:01:02 +0700

    init

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -151,7 +151,7 @@ tinymce.init({
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
+            <span>Copyright &copy; Patelki 2021</span>
           </div>
         </div>
       </footer>
